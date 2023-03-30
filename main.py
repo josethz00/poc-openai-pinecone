8035fe133e88ed359fb0ee13e83d005d9fd8e640
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-03-30 16:05:21 -0300

    feat: generate embeddings

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ load_dotenv()
 
 openai.api_key = os.getenv("OPEN_AI_API_KEY")
 openai.Engine.list()  # check we have authenticated
+
+MODEL = "text-embedding-ada-002"
+
+# Create an embedding for a single document using the text-embedding-ada-002 model
+res = openai.Embedding.create(
+    input=[
+        "Sample document text goes here",
+        "there will be several phrases in each batch"
+    ], 
+    engine=MODEL
+)
+
+embeds = [record["embedding"] for record in res["data"]]
